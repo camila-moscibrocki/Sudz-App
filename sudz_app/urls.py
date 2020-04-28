8974e4582c4d3026b33343bbe1b222adb6deb1a5
Camila Moscibrocki <camilamoscibrocki@gmail.com>
2020-04-28 19:12:17 -0300

    added bootstrapform

--- sudz_app/urls.py
+++ sudz_app/urls.py
@@ -6,10 +6,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
-
 urlpatterns = [
-    path('', include(home_urls)),
-    path('clientes/', include(clientes_urls)),
-    path('login/', auth_views.login, name='login'),
-    path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('', include(home_urls)),
+                  path('clientes/', include(clientes_urls)),
+                  path('login/', auth_views.LoginView.as_view(), name='login'),
+                  path('admin/', admin.site.urls),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
